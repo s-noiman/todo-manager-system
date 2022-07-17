a826cefe8be70d7c209b9e84a1e3ca8c81633fd9
Snoiman0047 <Snoiman0047@gmail.com>
2022-07-17 04:09:11 +0300

    Code first Migration to Seed Database.

---
+++ server/Migrations/202207162341172_Initial.cs
@@ -0,0 +1,29 @@
+﻿namespace TodoManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Todoes",
+                c => new
+                    {
+                        id = c.String(nullable: false, maxLength: 128),
+                        value = c.String(nullable: false),
+                        completed = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.id)
+                .Index(t => t.id, unique: true);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Todoes", new[] { "id" });
+            DropTable("dbo.Todoes");
+        }
+    }
+}
